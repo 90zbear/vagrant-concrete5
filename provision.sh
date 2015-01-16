3aaafbfc7d30517e197a0658d5ce3eae25da0a95
90zbear <pepper@stupid.jp>
2015-01-16 22:16:14 +0900

    c5downloaderをDLして展開。

--- provision.sh
+++ provision.sh
@@ -2,4 +2,4 @@ cd ansible
 sudo apt-get install -y python-dev python-pip
 sudo pip install ansible
 sudo pip install mySQL-python
-ansible-playbook playbook.yml -i hosts -c paramiko
+ansible-playbook playbook.yml -i hosts -c paramiko -vvv
